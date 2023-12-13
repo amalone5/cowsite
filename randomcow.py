de88ec42254a158d1fe8448044e679044fb91aca
jimcrankshaft <austin092317@gmail.com>
2023-12-13 04:05:12 +0000

    improved functionality, cleaned

---
+++ randomcow.py
@@ -0,0 +1,43 @@
+import random
+import subprocess
+import os
+
+cowpath="/var/www/html/cows.txt"
+index="/var/www/html/index.html"
+#index="index.html"
+
+
+def getCows():
+    cowfile = open(cowpath,"r")
+    cows = []
+    for cow in cowfile:
+        cows.append(cow.strip())
+    return cows
+
+
+def getRandomCow():
+    return random.choice(getCows())
+
+def buildtop():
+    os.system("echo '<!DOCTYPE html>' > "+index)
+    os.system("echo '<html lang=\"en\">' >> "+index)
+    os.system("echo '<head>' >> "+index)
+    os.system("echo '    <meta charset=\"UTF-8\">' >> "+index)
+    os.system("echo '    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">' >> "+index)
+    os.system("echo '    <title>Document</title>' >> "+index)
+    os.system("echo '</head>' >> "+index)
+    os.system("echo '<body>' >> "+index)
+
+def sendRandomCowToFile():
+    os.system("echo '<pre>' >> "+index)
+    os.system("fortune | cowsay -f "+getRandomCow()+" >> "+index)
+    os.system("echo '</pre>' >> "+index)
+
+def buildbottom():
+    os.system("echo '</body>' >> "+index)
+    os.system("echo '</html>' >> "+index)
+
+
+buildtop()
+sendRandomCowToFile()
+buildbottom()
--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo '' > /var/www/html/index.html
-echo '<p>' >> /var/www/html/index.html
-fortune >> /var/www/html/index.html
-echo '</p>' >> /var/www/html/index.html
-
