4c03a498ab06377c2634e2cde1fcaf5117f66ed1
Austin Malone <austin092317@gmail.com>
2023-12-13 15:37:20 -0500

    first commit

--- randomcow.py
+++ randomcow.py
@@ -24,7 +24,7 @@ def buildtop():
     os.system("echo '<head>' >> "+index)
     os.system("echo '    <meta charset=\"UTF-8\">' >> "+index)
     os.system("echo '    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">' >> "+index)
-    os.system("echo '    <title>Document</title>' >> "+index)
+    os.system("echo '    <title>Fortune</title>' >> "+index)
     os.system("echo '</head>' >> "+index)
     os.system("echo '<body>' >> "+index)
 
